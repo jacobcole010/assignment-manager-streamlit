af260abae28962b5719dd06a2b7b07b4781a448d
Jacob <jcole@udel.edu>
2026-03-10 21:20:06 -0400

    Start Streamlit - UIDesign

--- app.py
+++ app.py
@@ -1,14 +1,46 @@
 import streamlit as st
 
+#1. Step 1: Header First (Text Elements)
 st.title("AI Course Manager")
-st.write("Week 3 UI layer connected to the Week 2 Assignment Manager data model.")
+st.header("Course Assignments Manager")
+st.subheader("Course Assignments Manager")
 
-# Section 4: The Hierarchy of Information
-st.header("Current Assignments")
-st.subheader("Homework 1: ER Model")
-st.caption("Due: March 15, 2026")
+st.divider()
+#2. Step 2: Define Assigments List (Data Continuty)
 
-st.subheader("Lab 1: SQL Practice")
-st.caption("Due: March 20, 2026")
+assignments = [
+    {
+        "id" : "HW1",
+        "title" : "Intro to Database",
+        "description" : "Basics of Database Design",
+        "points" : 100,
+        "type" : "Homework"
+    } ,
+    {
+        "id" : "HW2",
+        "title" : "Normalization",
+        "description" : "Normalizing",
+        "points" : 100,
+        "type" : "Homework"
+    }
+]
 
-st.button("Click me")
+
+#3. Step 3: Add New Assignment Section (Inputs & Layout) 
+#col1, col2 = st.columns(2) ---> This will give you 50% - 50%
+st.subheader("Add New Assignment")
+
+with st.container(border = True):
+
+    col1, col2 = st.columns([2,1])
+    # col1, col2, col3 = st.columns([1,6,1])
+
+    with col1:
+        with st.container(border = True):
+            st.markdown(" ### Assignment Details")
+            title = st.text_input("Assignment Title", placeholder = "Homework 1", help = "Enter a Short Name")
+            description = st.text_area("Assignment Description", placeholder = "ex. details...")
+
+    with col2:
+        st.markdown("**Due Date Selection**")
+        due_date = st.date_input("Due Date")
