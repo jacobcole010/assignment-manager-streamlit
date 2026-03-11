16db34fc4a48100e5aabb43834dd9c79c4828eda
Jacob <jcole@udel.edu>
2026-03-10 22:08:57 -0400

    App Day 2

---
+++ app_day2.py
@@ -0,0 +1,53 @@
+import streamlit as st
+
+st.title("Course Management App")
+st.header("Assigment Management")
+st.subheader("Dashboard")
+
+st.divider()
+st.markdown("----------------")
+
+#Load Data
+assignments = [
+    {
+        "id" : "HW1",
+        "title" : "Intro to Database",
+        "description" : "Basics of Database Design",
+        "points" : 100,
+        "type" : "Homework"
+    } ,
+    {
+        "id" : "HW2",
+        "title" : "Normalization",
+        "description" : "Normalizing",
+        "points" : 100,
+        "type" : "Homework"
+    }
+]
+
+#Input
+
+#st.markdown("# Add New Assignment")
+st.markdown("## Add New Assignment")
+#st.markdown("### Add New Assignment")
+
+title = st.text_input("Title")
+description = st.text_area("Description", placeholder = "Normalization is Covered Here",
+                           help = "Here You are Entering the Assignment Details")
+points = st.number_input("Points")
+
+#assignment_type = st.text_input("Assignment Type")
+assignment_type = st.radio("Type", ["Homework", "Lab"], horizontal = True)
+st.caption("Homework Type")
+
+assignment_type2 = st.selectbox("Select an Option", ["Homework", "Lab", "Other"])
+if assignment_type2 == "Other":
+    assignment_type2 = st.text_input("Type", placeholder = "Enter the Assignment Type")
+
+due_date = st.date_input("Due Date")
+
+btn_save = st.button("Save Assignment", use_container_width=True)
+
+if btn_save:
+    st.warning("Working on it....")
+    
